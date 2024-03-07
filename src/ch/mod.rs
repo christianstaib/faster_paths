c543d8d0c8697b7c193b7e2a0f60aef6fe590c23
Christian Staib <staib.christian@hotmail.de>
2024-03-07 20:37:59 +0100

    added parallel contractor

--- src/ch/mod.rs
+++ src/ch/mod.rs
@@ -3,5 +3,6 @@ pub mod ch_queue;
 pub mod contraction_helper;
 pub mod contractor;
 pub mod fast_shortcut_replacer;
+pub mod parallel_contractor;
 pub mod shortcut;
 pub mod slow_shortcut_replacer;
---
+++ src/ch/parallel_contractor.rs
@@ -0,0 +1,130 @@
+use std::usize;
+
+use ahash::{HashMap, HashMapExt, HashSet};
+use indicatif::ProgressBar;
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use serde_derive::{Deserialize, Serialize};
+
+use crate::graphs::{edge::DirectedEdge, graph::Graph, types::VertexId};
+
+use super::{
+    ch_queue::{parallel_queue::ParallelQueue, queue::CHQueue},
+    shortcut::Shortcut,
+};
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct ContractedGraph {
+    pub graph: Graph,
+    pub shortcuts_map: Vec<(DirectedEdge, VertexId)>,
+    pub levels: Vec<Vec<u32>>,
+}
+
+pub struct ParallelContractor {
+    graph: Graph,
+    queue: ParallelQueue,
+    levels: Vec<u32>,
+}
+
+impl ParallelContractor {
+    pub fn new(graph: &Graph, priority_functions: &str) -> Self {
+        let levels = vec![0; graph.number_of_vertices() as usize];
+        let graph = graph.clone();
+        let queue = ParallelQueue::new(&graph, 3);
+
+        ParallelContractor {
+            graph,
+            queue,
+            levels,
+        }
+    }
+
+    pub fn get_graph(mut self) -> ContractedGraph {
+        let old_graph = self.graph.clone();
+
+        let shortcuts = self.contract_single_vertex();
+
+        self.graph = old_graph;
+        self.add_shortcuts(&shortcuts);
+        self.removing_edges_violating_level_property();
+
+        let shortcuts = shortcuts
+            .iter()
+            .map(|shortcut| (shortcut.edge.unweighted(), shortcut.skiped_vertex))
+            .collect();
+
+        let max_level = self.levels.iter().max().unwrap();
+        let mut levels = vec![Vec::new(); *max_level as usize + 1];
+
+        for (vertex, level) in self.levels.iter().enumerate() {
+            levels[*level as usize].push(vertex as u32);
+        }
+
+        ContractedGraph {
+            graph: self.graph,
+            shortcuts_map: shortcuts,
+            levels,
+        }
+    }
+
+    /// Generates contraction hierarchy where one vertex at a time is contracted.
+    pub fn contract_single_vertex(&mut self) -> Vec<Shortcut> {
+        let mut shortcuts = Vec::new();
+
+        let bar = ProgressBar::new(self.graph.number_of_vertices() as u64);
+
+        let mut level = 0;
+        while let Some(v) = self.queue.pop(&self.graph) {
+            let neighbors: HashSet<_> = v
+                .par_iter()
+                .map(|&(vertex, _)| self.graph.open_neighborhood(vertex, 1))
+                .flatten()
+                .collect();
+
+            for v in v {
+                let mut this_shortcuts = v.1;
+                let v = v.0;
+
+                self.add_shortcuts(&this_shortcuts);
+                shortcuts.append(&mut this_shortcuts);
+
+                self.graph.remove_vertex(v);
+                self.levels[v as usize] = level;
+            }
+
+            self.queue.update(&neighbors, &self.graph);
+
+            level += 1;
+            bar.inc(1);
+        }
+        bar.finish();
+
+        shortcuts
+    }
+
+    fn add_shortcuts(&mut self, shortcuts: &Vec<Shortcut>) {
+        shortcuts
+            .iter()
+            .cloned()
+            .for_each(|shortcut| self.graph.add_edge(&shortcut.edge));
+    }
+
+    fn removing_edges_violating_level_property(&mut self) {
+        let num_nodes = self.graph.number_of_vertices();
+        let mut out_edges: Vec<_> = (0..num_nodes)
+            .map(|tail| self.graph.out_edges(tail).clone())
+            .collect();
+        let mut in_edges: Vec<_> = (0..num_nodes)
+            .map(|tail| self.graph.in_edges(tail).clone())
+            .collect();
+
+        out_edges.iter_mut().enumerate().for_each(|(tail, edges)| {
+            edges.retain(|edge| self.levels[edge.head as usize] >= self.levels[tail as usize]);
+        });
+
+        in_edges.iter_mut().enumerate().for_each(|(head, edges)| {
+            edges.retain(|edge| self.levels[head as usize] <= self.levels[edge.tail as usize]);
+        });
+
+        self.graph = Graph::from_out_in_edges(out_edges, in_edges);
+    }
+}
