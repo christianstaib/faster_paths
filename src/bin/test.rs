851eae29458ea4afbf37b25efd80739e01e01420
Christian Staib <staib.christian@hotmail.de>
2024-03-11 22:15:23 +0100

    added test

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::BufReader};
+use std::{
+    fs::File,
+    io::BufReader,
+    time::{self, Duration, Instant},
+};
 
 use clap::Parser;
 use faster_paths::{
@@ -9,8 +13,12 @@ use faster_paths::{
         path::{PathFinding, ShortestPathValidation},
     },
     hl::hub_graph::HubGraph,
-    simple_algorithms::{bi_dijkstra::BiDijkstra, ch_bi_dijkstra::ChDijkstra, dijkstra::Dijkstra},
+    simple_algorithms::{
+        bi_dijkstra::BiDijkstra, ch_bi_dijkstra::ChDijkstra, dijkstra::Dijkstra,
+        slow_dijkstra::SlowDijkstra,
+    },
 };
+use indicatif::ProgressIterator;
 
 /// Starts a routing service on localhost:3030/route
 #[derive(Parser, Debug)]
@@ -33,8 +41,10 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let graph = GraphFactory::from_fmi_file(args.graph_path.as_str());
-    let graph = FastGraph::from_graph(&graph);
+    let slow_graph = GraphFactory::from_gr_file(args.graph_path.as_str());
+    let graph = FastGraph::from_graph(&slow_graph);
+
+    let slow_dijkstra = SlowDijkstra::new(&slow_graph);
 
     let dijkstra = Dijkstra::new(&graph);
 
@@ -42,45 +52,35 @@ fn main() {
 
     let reader = BufReader::new(File::open(args.ch_path).unwrap());
     let ch_graph: ContractedGraph = bincode::deserialize_from(reader).unwrap();
-    let ch_bi_dijkstra = ChDijkstra::new(&ch_graph);
+    let ch = ChDijkstra::new(&ch_graph);
 
     let reader = BufReader::new(File::open(args.hl_path).unwrap());
-    let hl_graph: HubGraph = bincode::deserialize_from(reader).unwrap();
+    let hl: HubGraph = bincode::deserialize_from(reader).unwrap();
 
     let reader = BufReader::new(File::open(args.tests_path).unwrap());
-    let requests: Vec<ShortestPathValidation> = serde_json::from_reader(reader).unwrap();
+    let validations: Vec<ShortestPathValidation> = serde_json::from_reader(reader).unwrap();
 
-    for request in requests.iter() {
-        let true_cost = request.weight;
-        let request = request.request.clone();
+    let mut path_finder: Vec<(&str, Box<dyn PathFinding>, Vec<Duration>)> = Vec::new();
+    path_finder.push(("dijkstra", Box::new(dijkstra), Vec::new()));
+    path_finder.push(("slow dijkstra", Box::new(slow_dijkstra), Vec::new()));
+    path_finder.push(("bi dijkstra", Box::new(bi_dijkstra), Vec::new()));
+    path_finder.push(("ch", Box::new(ch), Vec::new()));
+    path_finder.push(("hl", Box::new(hl), Vec::new()));
 
-        // test dijkstra
-        let response = dijkstra.get_shortest_path(&request);
-        let mut cost = None;
-        if let Some(route) = response {
-            cost = Some(route.weight);
+    for validation in validations.iter().take(100).progress() {
+        for (name, path_finder, times) in path_finder.iter_mut() {
+            let start = Instant::now();
+            let path = path_finder.get_shortest_path(&validation.request);
+            times.push(start.elapsed());
+
+            if let Err(err) = slow_graph.validate_path(&validation, &path) {
+                panic!("{} wrong: {}", name, err);
+            }
         }
-        assert_eq!(true_cost, cost, "dijkstra wrong");
+    }
 
-        // test bi dijkstra
-        let response = bi_dijkstra.get_shortest_path(&request);
-        let mut cost = None;
-        if let Some(route) = response {
-            cost = Some(route.weight);
-        }
-        assert_eq!(true_cost, cost, "bi dijkstra wrong");
-
-        // test ch dijkstra
-        let response = ch_bi_dijkstra.get_shortest_path(&request);
-        let mut cost = None;
-        if let Some(route) = response {
-            cost = Some(route.weight);
-        }
-        assert_eq!(true_cost, cost, "ch dijkstra wrong");
-
-        // test hl
-        let response = hl_graph.get_shortest_path_weight(&request);
-        let cost = response;
-        assert_eq!(true_cost, cost, "bi dijkstra wrong");
+    for (name, _, times) in path_finder.iter() {
+        let average = times.iter().sum::<Duration>() / times.len() as u32;
+        println!("{:<10} {:?}", name, average);
     }
 }
