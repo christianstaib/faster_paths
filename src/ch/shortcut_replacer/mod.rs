d24795e10c3159297d3b917a8a50a0df87261cda
Christian Staib <staib.christian@hotmail.de>
2024-03-16 14:14:59 +0100

    did stuff

--- src/ch/shortcut_replacer/mod.rs
+++ src/ch/shortcut_replacer/mod.rs
@@ -3,6 +3,6 @@ use crate::graphs::path::Path;
 pub mod fast_shortcut_replacer;
 pub mod slow_shortcut_replacer;
 
-pub trait ShortcutReplacer {
+pub trait ShortcutReplacer: Sync + Send {
     fn replace_shortcuts(&self, path: &Path) -> Path;
 }
