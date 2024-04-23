cc33a44b4e4c804ffbdcd7d3d93c26354376d02c
Christian Staib <staib.christian@hotmail.de>
2024-04-23 22:22:52 +0200

    did stuff

--- src/bin/create_top_down_hl.rs
+++ src/bin/create_top_down_hl.rs
@@ -7,10 +7,14 @@ use std::{
     usize,
 };
 
-use ahash::{HashSet, HashSetExt};
+use ahash::{HashMap, HashMapExt, HashSet, HashSetExt};
 use clap::Parser;
 use faster_paths::{
-    ch::{all_in_preprocessor::AllInPrerocessor, preprocessor::Preprocessor},
+    ch::{
+        all_in_preprocessor::AllInPrerocessor, preprocessor::Preprocessor,
+        shortcut_replacer::fast_shortcut_replacer::FastShortcutReplacer,
+    },
+    dijkstra_data::dijkstra_data_vec::DijkstraDataVec,
     graphs::{
         edge::DirectedWeightedEdge,
         graph_factory::GraphFactory,
@@ -27,7 +31,9 @@ use indicatif::{ParallelProgressIterator, ProgressIterator};
 use itertools::Itertools;
 
 use rand::prelude::*;
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelRefIterator, ParallelBridge, ParallelIterator,
+};
 
 /// Starts a routing service on localhost:3030/route
 #[derive(Parser, Debug)]
@@ -67,50 +73,56 @@ fn main() {
     let mut order = (0..graph.number_of_vertices()).collect_vec();
     order.shuffle(&mut rand::thread_rng());
 
-    let start = Instant::now();
-    let labels: Vec<_> = test_cases
+    let hub_graph = get_hl(&graph, &order);
+
+    test_cases
         .par_iter()
         .take(1_000)
         .progress()
-        .map(|test_case| {
-            let forward_label = get_out_label(test_case.request.source(), &graph, &order);
-            let backward_label = get_out_label(test_case.request.target(), &graph, &order);
-
-            let mut weight = None;
-            if let Some((this_weight, _, _)) = HubGraph::overlap(&forward_label, &backward_label) {
-                weight = Some(this_weight);
-            }
+        .for_each(|test_case| {
+            let weight = hub_graph.shortest_path_weight(&test_case.request);
 
             if weight != test_case.weight {
                 println!("err soll {:?}, ist {:?}", test_case.weight, weight);
             }
-            forward_label
-        })
-        .collect();
+        });
+
     println!("all {} tests passed", test_cases.len());
-
-    println!(
-        "average label size is {}",
-        labels
-            .iter()
-            .map(|label| label.entries.len())
-            .sum::<usize>()
-            / labels.len()
-    );
-
-    println!(
-        "took {:?} for {} labels which means {:?} for whole graph (half if bidirectional)",
-        start.elapsed(),
-        labels.len(),
-        start.elapsed() / labels.len() as u32 * graph.number_of_vertices()
-    );
 }
 
-fn get_out_label(vertex: VertexId, graph: &dyn Graph, order: &[u32]) -> Label {
+fn get_hl(graph: &dyn Graph, order: &[u32]) -> HubGraph {
     let dijkstra = Dijkstra::new(graph);
-    let data = dijkstra.single_source(vertex);
 
-    let mut children = vec![Vec::new(); graph.number_of_vertices() as usize];
+    let forward_labels: Vec<_> = (0..graph.number_of_vertices())
+        .progress()
+        .par_bridge()
+        .map(|source| {
+            let data = dijkstra.single_source(source);
+            get_label(source, &data, &order)
+        })
+        .collect();
+
+    let reverse_labels: Vec<_> = forward_labels.clone();
+    // let reverse_labels = (0..graph.number_of_vertices())
+    //     .progress()
+    //     .par_bridge()
+    //     .map(|source| {
+    //         let data = dijkstra.single_target(source);
+    //         get_label(source, &data, &order)
+    //     })
+    //     .collect();
+
+    HubGraph {
+        forward_labels,
+        reverse_labels,
+        shortcut_replacer: FastShortcutReplacer {
+            shortcuts: HashMap::new(),
+        },
+    }
+}
+
+fn get_label(vertex: VertexId, data: &DijkstraDataVec, order: &[u32]) -> Label {
+    let mut children = vec![Vec::new(); data.vertices.len()];
 
     for (vertex, entry) in data.vertices.iter().enumerate() {
         if let Some(predecessor) = entry.predecessor {
@@ -145,69 +157,4 @@ fn get_out_label(vertex: VertexId, graph: &dyn Graph, order: &[u32]) -> Label {
     label.entries.sort_unstable_by_key(|entry| entry.vertex);
 
     label
-}
-
-fn get_in_label(vertex: VertexId, graph: &dyn Graph, order: &[u32]) -> Label {
-    let dijkstra = Dijkstra::new(graph);
-    let data = dijkstra.single_target(vertex);
-
-    let mut children = vec![Vec::new(); graph.number_of_vertices() as usize];
-
-    for (vertex, entry) in data.vertices.iter().enumerate() {
-        if let Some(predecessor) = entry.predecessor {
-            children[predecessor as usize].push(vertex);
-        }
-    }
-
-    let mut stack = vec![vertex as usize];
-
-    let mut label = Label::new(vertex);
-    while let Some(current) = stack.pop() {
-        let mut current_children = std::mem::take(&mut children[current as usize]);
-
-        while let Some(child) = current_children.pop() {
-            let child_children = std::mem::take(&mut children[child]);
-
-            if order[child] > order[current] {
-                stack.extend(child_children);
-                label.entries.push(LabelEntry {
-                    vertex: child as VertexId,
-                    predecessor: Some(current as VertexId),
-                    weight: data.vertices[child].weight.unwrap(),
-                });
-            } else {
-                current_children.extend(child_children);
-            }
-        }
-    }
-
-    label.entries.sort_unstable_by_key(|entry| entry.vertex);
-
-    label
-}
-
-fn get_small_graph() -> ReversibleVecGraph {
-    // https://jlazarsfeld.github.io/ch.150.project/img/contraction/contract-full-1.png
-    let mut graph = ReversibleVecGraph::new();
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(0, 1, 3).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(0, 2, 5).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(0, 10, 3).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(1, 2, 3).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(1, 3, 5).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(2, 3, 2).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(2, 9, 2).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(3, 4, 7).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(3, 9, 4).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(4, 5, 6).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(4, 9, 3).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(5, 6, 4).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(5, 7, 2).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(6, 7, 3).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(6, 8, 5).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(7, 8, 3).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(7, 9, 2).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(8, 9, 4).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(8, 10, 6).unwrap());
-    add_edge_bidrectional(&mut graph, &DirectedWeightedEdge::new(9, 10, 3).unwrap());
-    graph
 }
