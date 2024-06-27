2f032726c2c1ce0783264672762cf303d3a5af16
Christian Staib <staib.christian@hotmail.de>
2024-06-27 11:19:12 +0200

    changed stuff

--- create_paths.sh
+++ create_paths.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 unset graph
 
 while getopts :g:d: flag; do
@@ -17,10 +15,10 @@ filename="${filename%.*}"
 data_path=ba_data/$filename
 tests_path=${data_path}/${filename}.${extension}.json
 num_seconds=$((2 * 24 * 60 * 60))
-num_paths=$((10 ** 6))
+num_paths=$((10 ** 7))
 
 mkdir -p $data_path
-sbatch -p single -n 40 -t 72:00:00 -j ${filename} \
-	create_paths -g $graph -n $num_paths -m $num_seconds -p $tests_path
 
+set -x
+sbatch -p single -n 40 -t 72:00:00 --job-name${filename} --output=ba_data/${filename}_create_paths.txt create_paths -g $graph -n $num_paths -m $num_seconds -p $tests_path
 set +x
