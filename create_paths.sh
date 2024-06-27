518808053c6ddb63b4cfe0ddb375e1d904e2beec
Christian Staib <staib.christian@hotmail.de>
2024-06-27 11:23:39 +0200

    did stuff

--- create_paths.sh
+++ create_paths.sh
@@ -20,5 +20,5 @@ num_paths=$((10 ** 7))
 mkdir -p $data_path
 
 set -x
-sbatch -p single -n 40 -t 72:00:00 --job-name=${filename} --output=ba_data/${filename}_create_paths.txt "create_paths -g $graph -n $num_paths -m $num_seconds -p $tests_path"
+sbatch -p single -n 40 -t 72:00:00 --job-name=${filename} --output=ba_data/${filename}_create_paths.txt --wrap="create_paths -g $graph -n $num_paths -m $num_seconds -p $tests_path"
 set +x
