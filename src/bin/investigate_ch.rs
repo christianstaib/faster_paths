fb39f7faabd5fe06fd1afa2f56f2d63cb00ce4bf
Christian Staib <staib.christian@hotmail.de>
2024-07-18 11:21:51 +0200

    did stuff

---
+++ src/bin/investigate_ch.rs
@@ -0,0 +1,44 @@
+use std::{fs::File, io::BufReader, path::PathBuf};
+
+use clap::Parser;
+use faster_paths::{
+    ch::directed_contracted_graph::DirectedContractedGraph,
+    graphs::{path::PathFinding, Graph},
+};
+
+/// Starts a routing service on localhost:3030/route
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Outfile in .bincode format
+    #[arg(short, long)]
+    contracted_graph: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("Loading contracted graph");
+    let reader = BufReader::new(File::open(&args.contracted_graph).unwrap());
+    let contracted_graph: DirectedContractedGraph = bincode::deserialize_from(reader).unwrap();
+
+    println!(
+        "the ch graph has {} vertices.",
+        contracted_graph.number_of_vertices()
+    );
+
+    println!(
+        "the upward graph has {} edges",
+        contracted_graph.upward_graph.number_of_edges()
+    );
+
+    println!(
+        "the downward graph has {} edges",
+        contracted_graph.downward_graph.number_of_edges()
+    );
+
+    println!(
+        "the ch graph contrains {} shortcuts",
+        contracted_graph.shortcuts.len()
+    );
+}
