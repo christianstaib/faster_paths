282ae5bec6f0c11ad3bcdddc741afbe2877664f1
Christian Staib <staib.christian@hotmail.de>
2024-08-31 15:19:30 +0200

    did stuff

---
+++ src/bin/ch_heuristic_hl.rs
@@ -0,0 +1,79 @@
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter},
+    path::PathBuf,
+};
+
+use clap::Parser;
+use faster_paths::{
+    graphs::{
+        read_edges_from_fmi_file, reversible_graph::ReversibleGraph, vec_vec_graph::VecVecGraph,
+        Distance, Vertex,
+    },
+    search::{
+        ch::contracted_graph::ContractedGraph, hl::hub_graph::HubGraph, DistanceHeuristic,
+        PathFinding,
+    },
+    utility::{benchmark_and_test, generate_test_cases},
+};
+
+/// Starts a routing service on localhost:3030/route
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Infile in .fmi format
+    #[arg(short, long)]
+    graph: PathBuf,
+    /// Infile in .fmi format
+    #[arg(short, long)]
+    hub_graph: PathBuf,
+    /// Infile in .fmi format
+    #[arg(short, long)]
+    contracted_graph: PathBuf,
+}
+
+pub struct PathfinderHeuristic<'a> {
+    pub pathfinder: &'a dyn PathFinding,
+}
+
+impl<'a> DistanceHeuristic for PathfinderHeuristic<'a> {
+    fn lower_bound(&self, source: Vertex, target: Vertex) -> Distance {
+        self.pathfinder
+            .shortest_path_distance(source, target)
+            .unwrap_or(0)
+    }
+
+    fn upper_bound(&self, source: Vertex, target: Vertex) -> Distance {
+        self.pathfinder
+            .shortest_path_distance(source, target)
+            .unwrap_or(Distance::MAX)
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // Build graph
+    let edges = read_edges_from_fmi_file(&args.graph);
+    let graph = ReversibleGraph::<VecVecGraph>::from_edges(&edges);
+
+    let reader = BufReader::new(File::open(&args.hub_graph).unwrap());
+    let hub_graph: HubGraph = bincode::deserialize_from(reader).unwrap();
+
+    let heuristic = PathfinderHeuristic {
+        pathfinder: &hub_graph,
+    };
+
+    // Create contracted_graph
+    let contracted_graph = ContractedGraph::by_contraction_with_heuristic(&graph, &heuristic);
+
+    // Write contracted_graph to file
+    let writer = BufWriter::new(File::create(&args.contracted_graph).unwrap());
+    bincode::serialize_into(writer, &contracted_graph).unwrap();
+
+    // Benchmark and test correctness
+    let tests = generate_test_cases(graph.out_graph(), 1_000);
+    let average_duration =
+        benchmark_and_test(graph.out_graph(), &tests, &contracted_graph).unwrap();
+    println!("Average duration was {:?}", average_duration);
+}
