74cd7dca8ede9ded300f92c2d3dd0e5d62ef2106
Christian Staib <staib.christian@hotmail.de>
2024-09-01 14:43:35 +0200

    did stuff

--- src/graphs/pathfinding.rs
+++ src/graphs/pathfinding.rs
@@ -1,11 +1,11 @@
-use super::{Distance, Graph, Vertex};
+use super::{reversible_graph::ReversibleGraph, Distance, Graph, Vertex};
 use crate::search::{
     collections::dijkstra_data::Path,
     dijkstra::{dijkstra_one_to_one_distance_wrapped, dijkstra_one_to_one_path_wrapped},
     PathFinding,
 };
 
-impl<T: Graph> PathFinding for T {
+impl<G: Graph> PathFinding for G {
     fn shortest_path(&self, source: Vertex, target: Vertex) -> Option<Path> {
         dijkstra_one_to_one_path_wrapped(self, source, target)
     }
@@ -19,6 +19,16 @@ impl<T: Graph> PathFinding for T {
     }
 }
 
-impl PathFind
-ing 
-f
+impl<G: Graph> PathFinding for ReversibleGraph<G> {
+    fn shortest_path(&self, source: Vertex, target: Vertex) -> Option<Path> {
+        self.out_graph().shortest_path(source, target)
+    }
+
+    fn shortest_path_distance(&self, source: Vertex, target: Vertex) -> Option<Distance> {
+        self.out_graph().shortest_path_distance(source, target)
+    }
+
+    fn number_of_vertices(&self) -> u32 {
+        self.out_graph().number_of_vertices()
+    }
+}
